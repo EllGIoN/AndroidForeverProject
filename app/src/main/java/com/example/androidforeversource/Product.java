662c2cbd12de6a22de9f94f2455c3074dfad2e1b
BeniaminKohnke <74410312+BeniaminKohnke@users.noreply.github.com>
2022-04-21 23:58:23 +0200

    RecordNameChange

---
+++ app/src/main/java/com/example/androidforeversource/Product.java
@@ -0,0 +1,9 @@
+package com.example.androidforeversource;
+
+public class Product {
+    public String sku;
+    public String name;
+    public String url;
+    public Double price;
+    public Double discountedPrice;
+}
