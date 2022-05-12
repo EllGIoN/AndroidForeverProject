c149a283c37dd869e644f6f1dca18a8195fbf2c8
BeniaminKohnke <74410312+BeniaminKohnke@users.noreply.github.com>
2022-05-12 14:53:25 +0200

    FixedDataAccess

--- app/src/main/java/com/example/androidforeversource/DataAccess.java
+++ app/src/main/java/com/example/androidforeversource/DataAccess.java
@@ -10,8 +10,9 @@ import android.util.Log;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
-import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.SuccessContinuation;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
@@ -23,18 +24,19 @@ public class DataAccess extends SQLiteOpenHelper {
     private static final int DATABASE_VERSION = 1;
     private static final String DATABASE_NAME = "androidForever";
     private static final String TABLE_PRODUCTS = "products";
-    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private static SQLiteDatabase sqldb;
 
     public DataAccess(@Nullable Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        sqldb = this.getReadableDatabase();
     }
 
     public void saveOrUpdateData(){
-        List<Product> products = db.collection("Products")
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        try {
+            Task<QuerySnapshot> dbTask = db.collection("Products")
+                    .get()
+                    .addOnCompleteListener(task -> {
                         if (task.isSuccessful()) {
                             for (QueryDocumentSnapshot document : task.getResult()) {
                                 Log.d("DATABASE", document.getId() + " => " + document.getData());
@@ -42,36 +44,58 @@ public class DataAccess extends SQLiteOpenHelper {
                         } else {
                             Log.w("DATABASE", "Error getting documents.", task.getException());
                         }
+                    });
+
+            dbTask.onSuccessTask(new SuccessContinuation<QuerySnapshot, Object>() {
+
+                @NonNull
+                @Override
+                public Task<Object> then(QuerySnapshot queryDocumentSnapshots) throws Exception {
+                    List<Product> products = new ArrayList<>();
+
+                    for (DocumentSnapshot document : queryDocumentSnapshots.getDocuments()) {
+                        Product product = document.toObject(Product.class);
+                        product.sku = document.getId();
+                        products.add(product);
                     }
-                }).getResult().toObjects(Product.class);
 
-        SQLiteDatabase db = this.getReadableDatabase();
-        for (Product product : products) {
-            ContentValues values = new ContentValues();
+                    for (Product product : products) {
+                        ContentValues values = new ContentValues();
 
-            values.put("sku", product.sku);
-            values.put("name", product.name);
-            values.put("url", product.url);
-            values.put("imageUrl", product.imageUrl);
-            values.put("currentPrice", product.currentPrice);
-            values.put("oldPrice", product.oldPrice);
-            values.put("category", product.category);
+                        values.put("sku", product.sku);
+                        values.put("name", product.name);
+                        values.put("url", product.url);
+                        values.put("imageUrl", product.imageUrl);
+                        values.put("currentPrice", product.currentPrice);
+                        values.put("oldPrice", product.oldPrice);
+                        values.put("category", product.category);
 
-            Cursor cursor = db.rawQuery("SELECT * FROM " + TABLE_PRODUCTS + "WHERE sku LIKE " + product.sku + ";", new String[]{});
-            if(cursor == null || !cursor.moveToFirst()){
-                db.insert(TABLE_PRODUCTS, null, values);
-            } else {
-                db.update(TABLE_PRODUCTS, values, "sku LIKE ?", new String[] { product.sku });
-            }
-            db.close();
+                        Cursor cursor = sqldb.rawQuery("SELECT * FROM " + TABLE_PRODUCTS + " WHERE sku LIKE '" + product.sku + "';", new String[]{});
+                        if(cursor == null || !cursor.moveToFirst()){
+                            sqldb.insert(TABLE_PRODUCTS, null, values);
+                        } else {
+                            sqldb.update(TABLE_PRODUCTS, values, "sku LIKE ?", new String[] { product.sku });
+                        }
+                        if(cursor != null){
+                            cursor.close();
+                        }
+                    }
+                    sqldb.close();
+                    return null;
+                }
+            });
+        } catch (Exception e){
+            e.printStackTrace();
+        } finally {
+            db.terminate();
         }
     }
 
     public ArrayList<Product> getProducts(){
         ArrayList<Product> products = new ArrayList<>();
-        SQLiteDatabase db = this.getReadableDatabase();
+        SQLiteDatabase sqldb = this.getReadableDatabase();
 
-        Cursor cursor = db.rawQuery("SELECT * FROM " + TABLE_PRODUCTS + ";", new String[]{});
+        Cursor cursor = sqldb.rawQuery("SELECT * FROM " + TABLE_PRODUCTS + ";", new String[]{});
         if(cursor != null){
             if(cursor.moveToFirst()) {
                 do{
@@ -92,15 +116,15 @@ public class DataAccess extends SQLiteOpenHelper {
     }
 
     @Override
-    public void onCreate(SQLiteDatabase db) {
+    public void onCreate(SQLiteDatabase sqldb) {
         String CREATE_PRODUCTS_TABLE = "CREATE TABLE " + TABLE_PRODUCTS
                 + " (id INTEGER PRIMARY KEY AUTOINCREMENT, sku TEXT, name TEXT, url TEXT, imageUrl TEXT, currentPrice REAL, oldPrice REAL, category TEXT);";
-        db.execSQL(CREATE_PRODUCTS_TABLE);
+        sqldb.execSQL(CREATE_PRODUCTS_TABLE);
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_PRODUCTS + ";");
-        onCreate(db);
+    public void onUpgrade(SQLiteDatabase sqldb, int oldVersion, int newVersion) {
+        sqldb.execSQL("DROP TABLE IF EXISTS " + TABLE_PRODUCTS + ";");
+        onCreate(sqldb);
     }
 }
