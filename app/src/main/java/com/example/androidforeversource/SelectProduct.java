b08428cf4b2d101001460fc7561b22dfbecd0625
BeniaminKohnke <74410312+BeniaminKohnke@users.noreply.github.com>
2022-05-20 01:00:16 +0200

    AddedProductFiltering

--- app/src/main/java/com/example/androidforeversource/SelectProduct.java
+++ app/src/main/java/com/example/androidforeversource/SelectProduct.java
@@ -2,8 +2,11 @@ package com.example.androidforeversource;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,38 +14,50 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SelectProduct extends AppCompatActivity {
-    //DataAccess db = new DataAccess(SelectProduct.this);
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_product);
+        updateProductList("");
 
-        addPositionsToView();
+        ListView productsView = (ListView) this.findViewById(R.id.ProductsList);
+        productsView.setOnItemClickListener((parent, view, position, id) -> {
+            String productSku = productsView.getAdapter().getItem(position).toString().split("Sku:")[1];
+            addNewEstimate.addProduct(new DataAccess(SelectProduct.this).getProduct(productSku));
+            finish();
+        });
     }
 
     public void updateProductData(View view){
         new DataAccess(SelectProduct.this).saveOrUpdateData();
-        addPositionsToView();
+        updateProductList("");
     }
 
-    private void addPositionsToView(){
+    public void updateProductList(View view){
+        EditText filterText = (EditText) this.findViewById(R.id.filterTextLabel);
+        updateProductList(filterText.getText().toString());
+    }
+
+    private void updateProductList(String filterText){
         List<Product> products = new DataAccess(SelectProduct.this).getProducts();
         List<String> productsStrings = new ArrayList<>();
 
         for (Product product : products) {
-            StringBuilder productString = new StringBuilder();
-            productString.append(product.name);
-            productString.append("\n\tCena:");
-            productString.append(product.currentPrice);
-            productString.append("\n\tPoprzednia cena:");
-            productString.append(product.oldPrice);
-            productString.append("\n\tŹródło:");
-            productString.append(product.url);
-            productsStrings.add(productString.toString());
-        };
-        ListView productsView = findViewById(R.id.ProductsList);
+            if(product.name.contains(filterText) || product.category.contains(filterText)){
+                String productString = product.name +
+                        "\n\tCena:" +
+                        product.currentPrice +
+                        "\n\tPoprzednia cena:" +
+                        product.oldPrice +
+                        "\n\tŹródło:" +
+                        product.url +
+                        "\n\tSku:" +
+                        product.sku;
+                productsStrings.add(productString);
+            }
+        }
 
+        ListView productsView = findViewById(R.id.ProductsList);
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>
                 (this, android.R.layout.simple_list_item_1, productsStrings);
 
