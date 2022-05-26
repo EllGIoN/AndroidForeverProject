1955480196d95ba73539f99e7b59f7659d8783c5
BeniaminKohnke <74410312+BeniaminKohnke@users.noreply.github.com>
2022-05-26 09:11:02 +0200

    AddedConnectionBetweenActivities

--- app/src/main/java/com/example/androidforeversource/DataAccess.java
+++ app/src/main/java/com/example/androidforeversource/DataAccess.java
@@ -7,10 +7,8 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
-import com.google.android.gms.tasks.SuccessContinuation;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -24,14 +22,16 @@ public class DataAccess extends SQLiteOpenHelper {
     private static final int DATABASE_VERSION = 1;
     private static final String DATABASE_NAME = "androidForever";
     private static final String TABLE_PRODUCTS = "products";
-    private static SQLiteDatabase sqldb;
+    private static final String TABLE_ESTIMATES = "estimates";
+    private static final String TABLE_ESTIMATES_PRODUCTS = "estimatesProducts";
+    private static SQLiteDatabase sqlDBInstance;
 
     public DataAccess(@Nullable Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
-        sqldb = this.getReadableDatabase();
     }
 
     public void saveOrUpdateData(){
+        sqlDBInstance = this.getReadableDatabase();
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         try {
             Task<QuerySnapshot> dbTask = db.collection("Products")
@@ -46,43 +46,34 @@ public class DataAccess extends SQLiteOpenHelper {
                         }
                     });
 
-            dbTask.onSuccessTask(new SuccessContinuation<QuerySnapshot, Object>() {
+            dbTask.onSuccessTask(queryDocumentSnapshots -> {
+                List<Product> products = new ArrayList<>();
 
-                @NonNull
-                @Override
-                public Task<Object> then(QuerySnapshot queryDocumentSnapshots) throws Exception {
-                    List<Product> products = new ArrayList<>();
-
-                    for (DocumentSnapshot document : queryDocumentSnapshots.getDocuments()) {
-                        Product product = document.toObject(Product.class);
-                        product.sku = document.getId();
-                        products.add(product);
-                    }
-
-                    for (Product product : products) {
-                        ContentValues values = new ContentValues();
-
-                        values.put("sku", product.sku);
-                        values.put("name", product.name);
-                        values.put("url", product.url);
-                        values.put("imageUrl", product.imageUrl);
-                        values.put("currentPrice", product.currentPrice);
-                        values.put("oldPrice", product.oldPrice);
-                        values.put("category", product.category);
-
-                        Cursor cursor = sqldb.rawQuery("SELECT * FROM " + TABLE_PRODUCTS + " WHERE sku LIKE '" + product.sku + "';", new String[]{});
-                        if(cursor == null || !cursor.moveToFirst()){
-                            sqldb.insert(TABLE_PRODUCTS, null, values);
-                        } else {
-                            sqldb.update(TABLE_PRODUCTS, values, "sku LIKE ?", new String[] { product.sku });
-                        }
-                        if(cursor != null){
-                            cursor.close();
-                        }
-                    }
-                    sqldb.close();
-                    return null;
+                for (DocumentSnapshot document : queryDocumentSnapshots.getDocuments()) {
+                    Product product = document.toObject(Product.class);
+                    product.sku = document.getId();
+                    products.add(product);
                 }
+
+                for (Product product : products) {
+                    ContentValues values = new ContentValues();
+
+                    values.put("sku", product.sku);
+                    values.put("name", product.name);
+                    values.put("url", product.url);
+                    values.put("imageUrl", product.imageUrl);
+                    values.put("currentPrice", product.currentPrice);
+                    values.put("oldPrice", product.oldPrice);
+                    values.put("category", product.category);
+
+                    try{
+                        sqlDBInstance.insert(TABLE_PRODUCTS, null, values);
+                    } catch (Exception e){
+                        e.printStackTrace();
+                    }
+                }
+                sqlDBInstance.close();
+                return null;
             });
         } catch (Exception e){
             e.printStackTrace();
@@ -92,12 +83,13 @@ public class DataAccess extends SQLiteOpenHelper {
     }
 
     public Product getProduct(String sku){
-        SQLiteDatabase sqldb = this.getReadableDatabase();
+        SQLiteDatabase sqlDB = this.getReadableDatabase();
 
-        Cursor cursor = sqldb.rawQuery("SELECT * FROM " + TABLE_PRODUCTS + " WHERE sku LIKE '" + sku + "';", new String[]{});
+        Cursor cursor = sqlDB.rawQuery("SELECT * FROM " + TABLE_PRODUCTS + " WHERE sku LIKE '" + sku + "';", new String[]{});
         if(cursor != null){
             if(cursor.moveToFirst()) {
                 Product product = new Product();
+                product.id = Integer.parseInt(cursor.getString(0));
                 product.sku = cursor.getString(1);
                 product.name = cursor.getString(2);
                 product.url = cursor.getString(3);
@@ -109,18 +101,20 @@ public class DataAccess extends SQLiteOpenHelper {
             }
             cursor.close();
         }
+        sqlDB.close();
         return null;
     }
 
     public ArrayList<Product> getProducts(){
         ArrayList<Product> products = new ArrayList<>();
-        SQLiteDatabase sqldb = this.getReadableDatabase();
+        SQLiteDatabase sqlDB = this.getReadableDatabase();
 
-        Cursor cursor = sqldb.rawQuery("SELECT * FROM " + TABLE_PRODUCTS + ";", new String[]{});
+        Cursor cursor = sqlDB.rawQuery("SELECT * FROM " + TABLE_PRODUCTS + ";", new String[]{});
         if(cursor != null){
             if(cursor.moveToFirst()) {
                 do{
                     Product product = new Product();
+                    product.id = Integer.parseInt(cursor.getString(0));
                     product.sku = cursor.getString(1);
                     product.name = cursor.getString(2);
                     product.url = cursor.getString(3);
@@ -133,7 +127,213 @@ public class DataAccess extends SQLiteOpenHelper {
             }
             cursor.close();
         }
+        sqlDB.close();
         return products;
+    }
+
+    public Estimate createEstimate(String name, String date){
+        SQLiteDatabase sqlDB = this.getReadableDatabase();
+        Estimate estimate = new Estimate();
+
+        Cursor cursor = sqlDB.rawQuery("SELECT * FROM " + TABLE_ESTIMATES + " WHERE name LIKE '" + name + "';", new String[]{});
+        if(cursor != null && cursor.moveToFirst()){
+            estimate.id = Integer.parseInt(cursor.getString(0));
+            estimate.name = cursor.getString(1);
+            estimate.date = cursor.getString(2);
+
+            cursor = sqlDB.rawQuery("SELECT p.* FROM " + TABLE_ESTIMATES + " AS e"
+                    + " JOIN " + TABLE_ESTIMATES_PRODUCTS + " AS ep ON ep.estimateId = e.id"
+                    + " JOIN " + TABLE_PRODUCTS + " AS p ON ep.productId = p.id"
+                    + " WHERE e.id = " + estimate.id + ";", new String[]{});
+            if(cursor != null && cursor.moveToFirst()){
+                do{
+                    Product product = new Product();
+                    product.id = Integer.parseInt(cursor.getString(0));
+                    product.sku = cursor.getString(1);
+                    product.name = cursor.getString(2);
+                    product.url = cursor.getString(3);
+                    product.imageUrl = cursor.getString(4);
+                    product.currentPrice = Double.parseDouble(cursor.getString(5));
+                    product.oldPrice = Double.parseDouble(cursor.getString(6));
+                    product.category = cursor.getString(7);
+                    estimate.products.add(product);
+                } while(cursor.moveToNext());
+            }
+        } else {
+            ContentValues values = new ContentValues();
+            values.put("name", name);
+            values.put("date", date);
+            sqlDB.insert(TABLE_ESTIMATES, null, values);
+
+            cursor = sqlDB.rawQuery("SELECT * FROM " + TABLE_ESTIMATES + " WHERE name LIKE '" + name + "';", new String[]{});
+            if(cursor != null && cursor.moveToFirst()){
+                estimate.id = Integer.parseInt(cursor.getString(0));
+                estimate.name = cursor.getString(1);
+                estimate.date = cursor.getString(2);
+            }
+        }
+
+        if(cursor != null){
+            cursor.close();
+        }
+        sqlDB.close();
+
+        return estimate.name.isEmpty() ? null : estimate;
+    }
+
+    public Estimate getEstimate(int id){
+        SQLiteDatabase sqlDB = this.getReadableDatabase();
+        Estimate estimate = new Estimate();
+
+        Cursor estimatesCursor = sqlDB.rawQuery("SELECT * FROM " + TABLE_ESTIMATES + " WHERE id = " + id + ";", new String[]{});
+        if(estimatesCursor != null && estimatesCursor.moveToFirst()){
+            estimate.id = Integer.parseInt(estimatesCursor.getString(0));
+            estimate.name = estimatesCursor.getString(1);
+            estimate.date = estimatesCursor.getString(2);
+
+            Cursor cursor = sqlDB.rawQuery("SELECT p.* FROM " + TABLE_ESTIMATES + " AS e"
+                    + " JOIN " + TABLE_ESTIMATES_PRODUCTS + " AS ep ON ep.estimateId = e.id"
+                    + " JOIN " + TABLE_PRODUCTS + " AS p ON ep.productId = p.id"
+                    + " WHERE e.id = " + estimate.id + ";", new String[]{});
+            if(cursor != null && cursor.moveToFirst()){
+                do{
+                    Product product = new Product();
+                    product.id = Integer.parseInt(cursor.getString(0));
+                    product.sku = cursor.getString(1);
+                    product.name = cursor.getString(2);
+                    product.url = cursor.getString(3);
+                    product.imageUrl = cursor.getString(4);
+                    product.currentPrice = Double.parseDouble(cursor.getString(5));
+                    product.oldPrice = Double.parseDouble(cursor.getString(6));
+                    product.category = cursor.getString(7);
+                    estimate.products.add(product);
+                } while(cursor.moveToNext());
+            }
+
+            if(cursor != null){
+                cursor.close();
+            }
+        }
+
+        if(estimatesCursor != null){
+            estimatesCursor.close();
+        }
+
+        sqlDB.close();
+        return estimate.name.isEmpty() ? null : estimate;
+    }
+
+    public Estimate getEstimate(String name){
+        SQLiteDatabase sqlDB = this.getReadableDatabase();
+        Estimate estimate = new Estimate();
+
+        Cursor estimatesCursor = sqlDB.rawQuery("SELECT * FROM " + TABLE_ESTIMATES + " WHERE name LIKE '" + name + "';", new String[]{});
+        if(estimatesCursor != null && estimatesCursor.moveToFirst()){
+            estimate.id = Integer.parseInt(estimatesCursor.getString(0));
+            estimate.name = estimatesCursor.getString(1);
+            estimate.date = estimatesCursor.getString(2);
+
+            Cursor cursor = sqlDB.rawQuery("SELECT p.* FROM " + TABLE_ESTIMATES + " AS e"
+                    + " JOIN " + TABLE_ESTIMATES_PRODUCTS + " AS ep ON ep.estimateId = e.id"
+                    + " JOIN " + TABLE_PRODUCTS + " AS p ON ep.productId = p.id"
+                    + " WHERE e.id = " + estimate.id + ";", new String[]{});
+            if(cursor != null && cursor.moveToFirst()){
+                do{
+                    Product product = new Product();
+                    product.id = Integer.parseInt(cursor.getString(0));
+                    product.sku = cursor.getString(1);
+                    product.name = cursor.getString(2);
+                    product.url = cursor.getString(3);
+                    product.imageUrl = cursor.getString(4);
+                    product.currentPrice = Double.parseDouble(cursor.getString(5));
+                    product.oldPrice = Double.parseDouble(cursor.getString(6));
+                    product.category = cursor.getString(7);
+                    estimate.products.add(product);
+                } while(cursor.moveToNext());
+            }
+
+            if(cursor != null){
+                cursor.close();
+            }
+        }
+
+        if(estimatesCursor != null){
+            estimatesCursor.close();
+        }
+
+        sqlDB.close();
+        return estimate.name.isEmpty() ? null : estimate;
+    }
+
+    public ArrayList<Estimate> getEstimates(){
+        SQLiteDatabase sqlDB = this.getReadableDatabase();
+        ArrayList<Estimate> estimates = new ArrayList<>();
+
+        Cursor estimatesCursor = sqlDB.rawQuery("SELECT * FROM " + TABLE_ESTIMATES + " ;", new String[]{});
+        if(estimatesCursor != null && estimatesCursor.moveToFirst()){
+            do{
+                Estimate estimate = new Estimate();
+                estimate.id = Integer.parseInt(estimatesCursor.getString(0));
+                estimate.name = estimatesCursor.getString(1);
+                estimate.date = estimatesCursor.getString(2);
+
+                Cursor cursor = sqlDB.rawQuery("SELECT p.* FROM " + TABLE_ESTIMATES + " AS e"
+                        + " JOIN " + TABLE_ESTIMATES_PRODUCTS + " AS ep ON ep.estimateId = e.id"
+                        + " JOIN " + TABLE_PRODUCTS + " AS p ON ep.productId = p.id"
+                        + " WHERE e.id = " + estimate.id + ";", new String[]{});
+                if(cursor != null && cursor.moveToFirst()){
+                    do{
+                        Product product = new Product();
+                        product.id = Integer.parseInt(cursor.getString(0));
+                        product.sku = cursor.getString(1);
+                        product.name = cursor.getString(2);
+                        product.url = cursor.getString(3);
+                        product.imageUrl = cursor.getString(4);
+                        product.currentPrice = Double.parseDouble(cursor.getString(5));
+                        product.oldPrice = Double.parseDouble(cursor.getString(6));
+                        product.category = cursor.getString(7);
+                        estimate.products.add(product);
+                    } while(cursor.moveToNext());
+                }
+
+                if(cursor != null){
+                    cursor.close();
+                }
+
+                estimates.add(estimate);
+            } while (estimatesCursor.moveToNext());
+        }
+
+        if(estimatesCursor != null){
+            estimatesCursor.close();
+        }
+
+        sqlDB.close();
+        return estimates;
+    }
+
+    public void addProductToEstimate(int estimateId, int productId){
+        SQLiteDatabase sqlDB = this.getReadableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put("estimateId", estimateId);
+        values.put("productId", productId);
+        sqlDB.insert(TABLE_ESTIMATES_PRODUCTS, null, values);
+
+        sqlDB.close();
+    }
+
+    public void removeProductFromEstimate(int estimateId, int productId){
+        SQLiteDatabase sqlDB = this.getReadableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put("estimateId", estimateId);
+        values.put("productId", productId);
+        sqlDB.rawQuery("DELETE FROM " + TABLE_ESTIMATES_PRODUCTS
+                + " WHERE id IN (SELECT id FROM " + TABLE_ESTIMATES_PRODUCTS
+                + " WHERE estimateId = " + estimateId + " AND productId = " + productId + " LIMIT 1)", new String[]{});
+
+        sqlDB.close();
     }
 
     @Override
@@ -141,11 +341,23 @@ public class DataAccess extends SQLiteOpenHelper {
         String CREATE_PRODUCTS_TABLE = "CREATE TABLE " + TABLE_PRODUCTS
                 + " (id INTEGER PRIMARY KEY AUTOINCREMENT, sku TEXT, name TEXT, url TEXT, imageUrl TEXT, currentPrice REAL, oldPrice REAL, category TEXT);";
         sqldb.execSQL(CREATE_PRODUCTS_TABLE);
+
+        String CREATE_ESTIMATES_TABLE = "CREATE TABLE " + TABLE_ESTIMATES
+                + " (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, date TEXT);";
+        sqldb.execSQL(CREATE_ESTIMATES_TABLE);
+
+        String CREATE_ESTIMATES_PRODUCTS = "CREATE TABLE " + TABLE_ESTIMATES_PRODUCTS
+                + " (id INTEGER PRIMARY KEY AUTOINCREMENT, estimateId INTEGER, productId INTEGER,"
+                + " FOREIGN KEY(estimateId) REFERENCES " + TABLE_ESTIMATES + ","
+                + " FOREIGN KEY(productId) REFERENCES " + TABLE_PRODUCTS + ");";
+        sqldb.execSQL(CREATE_ESTIMATES_PRODUCTS);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqldb, int oldVersion, int newVersion) {
         sqldb.execSQL("DROP TABLE IF EXISTS " + TABLE_PRODUCTS + ";");
+        sqldb.execSQL("DROP TABLE IF EXISTS " + TABLE_ESTIMATES + ";");
+        sqldb.execSQL("DROP TABLE IF EXISTS " + TABLE_ESTIMATES_PRODUCTS + ";");
         onCreate(sqldb);
     }
 }
---
+++ app/src/main/java/com/example/androidforeversource/Estimate.java
@@ -0,0 +1,11 @@
+package com.example.androidforeversource;
+
+import java.util.ArrayList;
+
+public class Estimate {
+    public int id;
+    public String name;
+    public String date;
+
+    ArrayList<Product> products = new ArrayList<>();
+}
